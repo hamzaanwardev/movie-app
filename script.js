c0c91298b26edf5e93ed6eb90ab179dd021430f8
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-13 23:38:02 +0500

    Fetched API

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&aqpi_key=8582442a21ce11db2105afa2f41e87d5';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8582442a21ce11db2105afa2f41e87d5&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500/';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=8582442a21ce11db2105afa2f41e87d5&query="';
