0b90cd9555b62e5f48fc9da9e00ab6ca587d79f2
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-14 21:56:28 +0500

    updated JavaScript

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8582442a21ce11db2105afa2f41e87d5&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500/';
-const SEARCH_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=8582442a21ce11db2105afa2f41e87d5&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?api_key=8582442a21ce11db2105afa2f41e87d5&query="';
 
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getMovies(API_URL)
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log (data.results)
+    showMovies (data.results)
 }
 
-form.addEventListener('siubmit', (e) => {
+function showMovies (movies) {
+    main.innerHTML = '';
+
+    main.forEach((movie) => {
+        const {title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movie.innerHTML = `
+        <div class="movie">
+        <img src="images/kevin-mueller-8F9cZvZyU7Q-unsplash.jpg" alt="">
+        <div class="movie-info">
+            <h3>Movies Title</h3>
+            <span class="green">9.8</span>
+        </div>
+        <div class="overview">
+            <h3>OverView</h3>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id alias ullam odio maxime!
+        </div>
+    </div>
+        `
+    })
+}
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchTerm = search.value
 
     if(searchTerm && searchTerm !== '') {
-        getMovies()
-    }
-})
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value=''
+        }else{
+            window.location.reload()
+    };
+});
